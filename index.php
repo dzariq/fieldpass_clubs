3df6d2483ab945ce44fb7b3c1a302a46df943642
Dzariq <dzariq85@gmail.com>
2024-02-09 18:56:45 +0800

    a

--- index.php
+++ index.php
@@ -1,41 +1,28 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+use Google\Cloud\PubSub\PubSubClient;
 
-// RabbitMQ connection parameters
-$host = 'b-3689f904-84c1-4b17-af1c-5a88ca79dac2.mq.us-east-1.amazonaws.com';
-$port = 5671;
-$username = 'fieldpass';
-$password = '#1Sampai9123456';
-$vhost = '/';
-$exchange = 'fieldpass_clubs';
-$queue = 'new-club';
+// Create a Pub/Sub client
+$pubsub = new PubSubClient([
+    'projectId' => 'chatbot-401803'
+]);
 
-// Create a connection to RabbitMQ
-$connection = new AMQPStreamConnection($host, $port, $username, $password, $vhost);
+// Get the topic name
+$topicName = 'new-club';
 
-// Create a channel
-$channel = $connection->channel();
+// Get the topic
+$topic = $pubsub->topic($topicName);
 
-// Declare the exchange and queue
-$channel->exchange_declare($exchange, 'direct', false, true, false);
-$channel->queue_declare($queue, false, true, false, false);
-$channel->queue_bind($queue, $exchange);
+// Create a message to publish
+$data = [
+    'message' => 'Hello from Cloud Run!'
+];
 
-// Message to publish
-$messageBody = 'Hello from PHP!';
+// Publish the message
+$topic->publish([
+    'data' => json_encode($data)
+]);
 
-// Create a new AMQPMessage with the message body
-$message = new AMQPMessage($messageBody);
-
-// Publish the message to the exchange
-$channel->basic_publish($message, $exchange);
-
-echo "Message published successfully.\n";
-
-// Close the channel and connection
-$channel->close();
-$connection->close();
+echo "Event published successfully.\n";
