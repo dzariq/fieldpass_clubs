692e06731d951e33f045818e32b895e2a760db55
Dzariq <dzariq85@gmail.com>
2024-02-10 22:28:41 +0800

    a

--- app.js
+++ app.js
@@ -7,44 +7,46 @@
 const express = require('express');
 const app = express();
 const { PubSub } = require('@google-cloud/pubsub');
+const { GoogleAuth } = require('google-auth-library');
 
-// Create a new Pub/Sub client
-const pubsub = new PubSub({
-  projectId: 'chatbot-401803',
-});
-// Define the topic name
 const topicName = 'projects/chatbot-401803/topics/new-club';
+const keyFilePath = 'fieldpass.privatekey.json';
 
-// This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
-// [END run_pubsub_server_setup]
-// [END cloudrun_pubsub_server_setup]
-
-// [START cloudrun_pubsub_handler]
-// [START run_pubsub_handler]
 app.post('/', (req, res) => {
-  // Call the publishMessage function to publish the message
   publishMessage();
 
   res.status(200).send(`OK`);
   return;
 });
 
-// Publish a message to the topic
 async function publishMessage() {
   const data = JSON.stringify({ message: 'Hello from CLUBS!' });
 
   try {
-    // Get the topic object
-    const topic = pubsub.topic(topicName);
+    const client = await authenticateWithServiceAccount();
 
-    // Publish the message
+    const pubsub = new PubSub({
+      projectId: 'chatbot-401803',
+    });
+
+    const topic = pubsub.topic(topicName);
     const messageId = await topic.publish(Buffer.from(data));
 
     console.log(`Message ${messageId} published.`);
   } catch (error) {
     console.error('Error publishing message:', error);
   }
+}
+
+async function authenticateWithServiceAccount() {
+  const auth = new GoogleAuth({
+      keyFilename: keyFilePath,
+      scopes: ['https://www.googleapis.com/auth/cloud-platform']
+  });
+
+  const client = await auth.getClient();
+  return client;
 }
 
 // [END run_pubsub_handler]
