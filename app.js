f717aa9cd1ab69a7a1aef53ede4b6e941ddac7f1
Dzariq <dzariq85@gmail.com>
2024-02-10 10:45:24 +0800

    aa

---
+++ app.js
@@ -0,0 +1,41 @@
+// Copyright 2020 Google LLC. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// [START cloudrun_pubsub_server_setup]
+// [START run_pubsub_server_setup]
+const express = require('express');
+const app = express();
+
+const { PubSub } = require('@google-cloud/pubsub');
+
+// Create a new Pub/Sub client
+const pubsub = new PubSub({
+  projectId: 'chatbot-401803',
+});
+
+// Define the topic name
+const topicName = 'new-club';
+
+// Publish a message to the topic
+async function publishMessage() {
+  const data = JSON.stringify({ message: 'Hello from CLUBS!' });
+
+  try {
+    // Get the topic object
+    const topic = pubsub.topic(topicName);
+
+    // Publish the message
+    const messageId = await topic.publish(Buffer.from(data));
+
+    console.log(`Message ${messageId} published.`);
+  } catch (error) {
+    console.error('Error publishing message:', error);
+  }
+}
+
+// Call the publishMessage function to publish the message
+publishMessage();
+
+
+module.exports = app;
