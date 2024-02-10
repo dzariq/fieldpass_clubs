3f80aa8ad49842160234022c568aadba1c2352e7
Dzariq <dzariq85@gmail.com>
2024-02-10 16:09:23 +0800

    a

--- app.js
+++ app.js
@@ -7,24 +7,14 @@
 const express = require('express');
 const app = express();
 const { PubSub } = require('@google-cloud/pubsub');
-
-// Create a new Pub/Sub client
-const pubsub = new PubSub({
-  projectId: 'chatbot-401803',
-});
+const pubsub = new PubSub();
 
 // Define the topic name
 const topicName = 'new-club';
 
-// This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
-// [END run_pubsub_server_setup]
-// [END cloudrun_pubsub_server_setup]
 
-// [START cloudrun_pubsub_handler]
-// [START run_pubsub_handler]
 app.post('/', (req, res) => {
-  // Call the publishMessage function to publish the message
   publishMessage();
 
   res.status(200).send(`OK`);
@@ -33,22 +23,23 @@ app.post('/', (req, res) => {
 
 // Publish a message to the topic
 async function publishMessage() {
-  const data = JSON.stringify({ message: 'Hello from CLUBS!' });
+   // Message to publish
+   const data = JSON.stringify({ message: 'Hello from CLUB MICROSERVICE!' });
 
-  try {
-    // Get the topic object
-    const topic = pubsub.topic(topicName);
-
-    // Publish the message
-    const messageId = await topic.publish(Buffer.from(data));
-
-    console.log(`Message ${messageId} published.`);
-  } catch (error) {
-    console.error('Error publishing message:', error);
-  }
+   try {
+     // Get the Pub/Sub topic
+     const topic = pubsub.topic(topicName);
+ 
+     // Publish the message to the topic
+     await topic.publish(Buffer.from(data));
+ 
+     res.status(200).send('Message published to Pub/Sub topic.');
+   } catch (error) {
+     console.error('Error publishing message:', error);
+     res.status(500).send('Error publishing message to Pub/Sub topic.');
+   }
 }
 
-// [END run_pubsub_handler]
-// [END cloudrun_pubsub_handler]
+
 
 module.exports = app;
