f20dd1af4f5d88732b36ecd3fe812162c01f1223
Dzariq <dzariq85@gmail.com>
2024-02-10 10:49:56 +0800

    aa

--- app.js
+++ app.js
@@ -7,35 +7,46 @@
 const express = require('express');
 const app = express();
 
-const { PubSub } = require('@google-cloud/pubsub');
+// This middleware is available in Express v4.16.0 onwards
+app.use(express.json());
+// [END run_pubsub_server_setup]
+// [END cloudrun_pubsub_server_setup]
 
-// Create a new Pub/Sub client
-const pubsub = new PubSub({
-  projectId: 'chatbot-401803',
-});
+// [START cloudrun_pubsub_handler]
+// [START run_pubsub_handler]
+app.post('/', (req, res) => {
 
-// Define the topic name
-const topicName = 'new-club';
+  const { PubSub } = require('@google-cloud/pubsub');
 
-// Publish a message to the topic
-async function publishMessage() {
-  const data = JSON.stringify({ message: 'Hello from CLUBS!' });
+  // Create a new Pub/Sub client
+  const pubsub = new PubSub({
+    projectId: 'chatbot-401803',
+  });
 
-  try {
-    // Get the topic object
-    const topic = pubsub.topic(topicName);
+  // Define the topic name
+  const topicName = 'new-club';
 
-    // Publish the message
-    const messageId = await topic.publish(Buffer.from(data));
+  // Publish a message to the topic
+  async function publishMessage() {
+    const data = JSON.stringify({ message: 'Hello from CLUBS!' });
 
-    console.log(`Message ${messageId} published.`);
-  } catch (error) {
-    console.error('Error publishing message:', error);
+    try {
+      // Get the topic object
+      const topic = pubsub.topic(topicName);
+
+      // Publish the message
+      const messageId = await topic.publish(Buffer.from(data));
+
+      console.log(`Message ${messageId} published.`);
+    } catch (error) {
+      console.error('Error publishing message:', error);
+    }
   }
-}
 
-// Call the publishMessage function to publish the message
-publishMessage();
-
+  // Call the publishMessage function to publish the message
+  publishMessage();
+});
+// [END run_pubsub_handler]
+// [END cloudrun_pubsub_handler]
 
 module.exports = app;
